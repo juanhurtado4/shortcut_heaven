903c4de02a85f1accfcc34299a46d8370f29bd2b
Juan Hurtado <juanhurtado4@outlook.com>
2018-05-26 23:52:40 -0700

    Feat: Copy shorcut event demo

---
+++ src/components/editor/active_line.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import hotkeys from 'hotkeys-js';
+
+class HotKeysDemo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: 'Trying this first',
+        }
+        hotkeys('cmd+shift+d', (e, handle) => this.onCopySC(e, handle))
+    }
+    onCopySC(e, handle) {
+        e.preventDefault();
+        this.setState({output: 'cmd was pressed'});
+        console.log(handle);
+    }
+
+    render() {
+        return (
+            <div>
+                <div > {this.state.output} </div>
+            </div>
+        )
+    }
+}
+
+// hotkeys('cmd', () => console.log('hello'));
+
+export default HotKeysDemo;
