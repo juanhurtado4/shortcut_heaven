927c3b1ecb18c77080f07c1adbb18f72a9ade155
Juan Hurtado <juanhurtado4@outlook.com>
2018-05-28 14:44:47 -0700

    Feat: Import and render initial lesson nav component

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+// import LessonTips from "../lesson_tips/lesson_tips";
+import LessonNavigation from "../lesson_nav/lesson_nav";
+import interfaceProps from "../interface/interface_props";
 
 class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lesson: 1
+            lesson: 1,
+            currentInterface: interfaceProps.atom,
         }
 
     }
@@ -15,15 +19,23 @@ class Game extends Component {
         })
     }
 
+    onChangeInterface(interfaceName) {
+        this.setState({
+            currentInterface: interfaceName
+        })
+    }
+
     render() {
         return (
             <div className="game">
-                <LessonTips/>
-                <Editor/>
-                <LessonNavigation/>
+                <LessonNavigation currentInterface={this.state.currentInterface}/>
+                {/*<LessonTips interface={this.state.interface.atom}/>*/}
+                {/*<Editor/>*/}
             </div>
         )
     }
 
 
 }
+
+export default Game;
