62732b675081d8e16167c41705a8575ea7947641
Juan Hurtado <juanhurtado4@outlook.com>
2018-06-10 21:48:29 -0700

    Refactor: Change stateless component into a functional one

--- src/components/lesson_nav/lesson.js
+++ src/components/lesson_nav/lesson.js
@@ -1,23 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Lesson extends Component {
-    constructor(props) {
-        super(props);
-    }
+/**
+ * Creates a single lesson component for navigation
+ * @return {HTMLElement}
+ */
+const Lesson = ( { lesson } ) => (
+    <div className="lessons_nav__lesson">
+        <div className="lessons_nav__lesson__status"></div>
+        <a className="lessons_nav__lesson__title">{lesson}</a>
+    </div>
+)
 
-    /**
-     * Creates a single lesson component for navigation
-     * @return {HTMLElement}
-     */
-    render() {
-        return (
-            <div className="lessons_nav__lesson">
-                <div className="lessons_nav__lesson__status"></div>
-                <a className="lessons_nav__lesson__title">{this.props.lesson}</a>
-            </div>
-        )
-    }
-
-}
 
 export default Lesson;
