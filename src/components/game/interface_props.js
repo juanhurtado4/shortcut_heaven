4e0a9783a13d1a8fd6e09d5992f221c3a9dc3cfb
Juan Hurtado <juanhurtado4@outlook.com>
2018-06-11 23:56:17 -0700

    Feat: Add correct interface props

--- src/components/game/interface_props.js
+++ src/components/game/interface_props.js
@@ -1,24 +1,9 @@
 /**
  * Holds the properties for all the interfaces
  */
-const interfaceProps = {
-    atom: {
-        name: 'Atom',
-        type: 'editor',
-        shortcuts: {
-            sc1: 'Duplicate Line',
-            sc2: 'Move Line',
-            sc3: 'Cut Line',
-            sc4: 'Delete from cursor onward',
-            sc5: 'Multiple cursors',
-        }
-    }
-};
-
-// TODO: Change interface props to this model & all props to follow new object
 // const interfaceProps = {
-//     editor: {
-//         names: ['Atom', 'VS Code', 'Sublime Text', 'Webstorm'],
+//     atom: {
+//         name: 'Atom',
 //         type: 'editor',
 //         shortcuts: {
 //             sc1: 'Duplicate Line',
@@ -26,12 +11,27 @@ const interfaceProps = {
 //             sc3: 'Cut Line',
 //             sc4: 'Delete from cursor onward',
 //             sc5: 'Multiple cursors',
-//         },
-//
-//     },
-//     browser: {
-//
+//         }
 //     }
-// };
+};
+
+// TODO: Change interface props to this model & all props to follow new object
+const interfaceProps = {
+    editor: {
+        names: ['Atom', 'VS Code', 'Sublime Text', 'Webstorm'],
+        type: 'editor',
+        shortcuts: {
+            sc1: 'Duplicate Line',
+            sc2: 'Move Line',
+            sc3: 'Cut Line',
+            sc4: 'Delete from cursor onward',
+            sc5: 'Multiple cursors',
+        },
+
+    },
+    browser: {
+
+    }
+};
 
 export default interfaceProps;
