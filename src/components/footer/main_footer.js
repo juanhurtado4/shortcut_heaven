234fbadbb0a7f81f1e6108fa68e12cfd4565608c
juan hurtado <juanhurtado4@outlook.com>
2018-07-24 12:24:49 -0700

    feat: Add game's footer component

---
+++ src/components/footer/main_footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainFooter = () => (
+        <footer className="game__footer">
+            <span className="contact">Get in touch!</span>
+            <i className="fab fa-github"></i>
+            <i className="fab fa-linkedin-in"></i>
+            <i className="fab fa-twitter"></i>
+            <i className="far fa-envelope-open"></i>
+        </footer>
+);
+
+export default MainFooter;
