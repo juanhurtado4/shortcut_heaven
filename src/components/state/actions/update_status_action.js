3e2d8bfc565329d3e019e5af9b610c19709e2fde
juan hurtado <juanhurtado4@outlook.com>
2018-07-25 14:21:32 -0700

    refactor: Pass id to modal & status actions

--- src/components/state/actions/update_status_action.js
+++ src/components/state/actions/update_status_action.js
@@ -1,9 +1,10 @@
 import { UPDATE_STATUS } from "../action_types/action_types";
 
-const updateStatusAction = () => {
+const updateStatusAction = (id) => {
     return {
         type: UPDATE_STATUS,
-        status: true
+        status: true,
+        id,
     }
 };
 
--- src/components/state/events/keyboard_events.js
+++ src/components/state/events/keyboard_events.js
@@ -2,6 +2,7 @@ import gameLessons from "../../main_game/game_lessons";
 import store from "../store/store";
 import duplicateAction from "../actions/duplicate_action";
 import updateStatusAction from "../actions/update_status_action";
+import updateModalAction from "../actions/update_modal_action";
 
 const onDuplicateAnswer = (event) => {
     event.preventDefault();
@@ -9,7 +10,8 @@ const onDuplicateAnswer = (event) => {
         gameLessons.duplicateLine.solvedCode,
         gameLessons.duplicateLine.lines.solved,
     ));
-    store.dispatch(updateStatusAction());
+    store.dispatch(updateStatusAction(gameLessons.duplicateLine.id));
+    store.dispatch(updateModalAction(gameLessons.duplicateLine.id));
 };
 
 export default onDuplicateAnswer;
