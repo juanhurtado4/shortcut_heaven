f2689dac80062fc3ed978791038c2ffec9dffc29
juan hurtado <juanhurtado4@outlook.com>
2018-07-25 14:24:06 -0700

    refactor: Change reducer name to camel case

--- src/components/state/reducers/editor_reducer.js
+++ src/components/state/reducers/editor_reducer.js
@@ -1,19 +1,17 @@
 import { DUPLICATE_LINE } from "../action_types/action_types";
 import gameLessons from "../../main_game/game_lessons";
 
-const editor_reducer = (state = {}, action) => {
+const editorReducer = (state = {}, action) => {
     switch(action.type) {
         case DUPLICATE_LINE:
-            gameLessons.duplicateLine.status = true;
             return {
                 ...state,
                 code: action.code,
                 lines: action.lines,
-                status: gameLessons.duplicateLine.status,
             };
         default :
             return state
     }
 };
 
-export default editor_reducer;
+export default editorReducer;
